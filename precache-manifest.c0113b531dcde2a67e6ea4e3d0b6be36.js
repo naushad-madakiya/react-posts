9792acb7533ab66d2bc3f6a96cdaa4e6a6328094
Naushad Madakiya <naushad@rosterbuster.com>
2019-05-26 14:17:02 +0530

    Updates

---
+++ precache-manifest.c0113b531dcde2a67e6ea4e3d0b6be36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aacda3d396c1e23f27bb3a07fb25043",
+    "url": "/react-posts/index.html"
+  },
+  {
+    "revision": "8ac125ae04efb0615a58",
+    "url": "/react-posts/static/css/main.440fe2fa.chunk.css"
+  },
+  {
+    "revision": "ef6e8e4bddf350fb2c84",
+    "url": "/react-posts/static/js/2.a3649772.chunk.js"
+  },
+  {
+    "revision": "8ac125ae04efb0615a58",
+    "url": "/react-posts/static/js/main.89b61dc4.chunk.js"
+  },
+  {
+    "revision": "5976bc637b10d6bfaf6b",
+    "url": "/react-posts/static/js/runtime~main.9fa446bd.js"
+  }
+]);
